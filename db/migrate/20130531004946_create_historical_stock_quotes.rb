ee3f6cb56412b81bcacb5aef133737db284cb5f7
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-01-22 22:35:28 +0000

    New buttons

--- db/migrate/20130531004946_create_historical_stock_quotes.rb
+++ db/migrate/20130531004946_create_historical_stock_quotes.rb
@@ -6,3 +6,11 @@ class CreateHistoricalStockQuotes < ActiveRecord::Migration
       t.string  :origin_code
       t.date    :created_date
       t.integer :stock_quotes_qty
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :historical_stock_quotes
+  end
+end
