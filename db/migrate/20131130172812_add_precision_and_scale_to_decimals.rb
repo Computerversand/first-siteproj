6455ac93a0ab52ff4878e207ef2b3d050e731239
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-04-20 11:31:38 +0000

    qseecom: fix secure app loading failure by kernel mode qseecom client

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -1,3 +1,3 @@
 class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
   def self.up
-    change_column :stock_quotes, :opening_price, :decimal, :prec
+    change_column :stock_quotes, :opening_price, :decimal, :precision => 10, :scal
