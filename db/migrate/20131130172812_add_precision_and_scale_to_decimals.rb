976e786518106874a5ca36f14c6531dc7d066b7f
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-05-18 13:29:38 +0000

    add font

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -4,4 +4,4 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :highest_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :lowest_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :average_price, :decimal, :precision => 10, :scale => 2
-    change_column :stock_quotes, :c
+    change_column :stock_quotes, :close_price, 
