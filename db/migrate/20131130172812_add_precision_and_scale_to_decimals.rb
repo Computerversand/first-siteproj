66faf34d4ba0622aa27e7af46bce4e1868a4e0bc
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-05-26 08:21:27 +0000

    added jquery

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -5,4 +5,4 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :lowest_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :average_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :close_price, :decimal, :precision => 10, :scale => 2
-    change_column :stock_quotes, :best_purchase_price, :decimal, 
+    change_column :stock_quotes, :best_purchase_price, :decimal, :precisio
