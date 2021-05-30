34b6950cbc5d360bd290142959caa8e8f4d4f01c
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-05-30 14:45:23 +0000

    improve tests

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -6,4 +6,4 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :average_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :close_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :best_purchase_price, :decimal, :precision => 10, :scale => 2
-    chang
+    change_column :stock_quo
