1376a550c56a95e9cc3013b42a1639164d84c4b0
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-06-03 15:08:00 +0000

    can: kvaser_usb: Fix state handling upon BUS_ERROR events

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -7,4 +7,4 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :close_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :best_purchase_price, :decimal, :precision => 10, :scale => 2
     change_column :stock_quotes, :best_sell_price, :decimal, :precision => 10, :scale => 2
- 
+    change_column 
