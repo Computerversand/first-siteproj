d0bf1f9921d7a418e393b24e1166ec4e7baf00e4
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-07-17 18:38:38 +0000

    faces

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -18,4 +18,5 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :lowest_price, :decimal
     change_column :stock_quotes, :average_price, :decimal
     change_column :stock_quotes, :close_price, :decimal
-    change_column :stock_quotes, :best_purchase_price, :deci
+    change_column :stock_quotes, :best_purchase_price, :decimal
+    change_colum
