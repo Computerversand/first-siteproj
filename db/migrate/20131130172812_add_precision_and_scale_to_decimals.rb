10fc4fbdffa41991f1d85e0c28658d01dc221361
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-08-01 07:22:39 +0000

    clean gitignore

--- db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
+++ db/migrate/20131130172812_add_precision_and_scale_to_decimals.rb
@@ -22,4 +22,4 @@ class AddPrecisionAndScaleToDecimals < ActiveRecord::Migration
     change_column :stock_quotes, :best_sell_price, :decimal
     change_column :stock_quotes, :total_trading, :decimal
     change_column :stock_quotes, :total_trading_share, :decimal
-    cha
+    change_column :stock_q
