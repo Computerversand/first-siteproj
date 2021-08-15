7f1cbe2490c8afdef44f9b84a064287add533615
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-08-15 06:15:52 +0000

    remove temp file

--- db/schema.rb
+++ db/schema.rb
@@ -3,4 +3,4 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definit
+# Note that this schema.rb definition is the authoritative 
