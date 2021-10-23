654b8460aee4d6d2d9f2124ea6486c741e727eca
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-10-23 12:39:41 +0000

    Fix merge mistake

--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,5 @@ ActiveRecord::Schema.define(:version => 20131130172812) do
   create_table "historical_stock_quotes", :force => true do |t|
     t.string   "filename"
     t.string   "origin_code"
-    t.date     "created_
+    t.date     "created_date"
+    t.in
