5bfc0bdd3bbd0eeb3404e0f44b0b1cefeec90a90
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-10-31 06:48:38 +0000

    fuck me

--- db/schema.rb
+++ db/schema.rb
@@ -22,4 +22,4 @@ ActiveRecord::Schema.define(:version => 20131130172812) do
     t.datetime "updated_at",       :null => false
   end
 
-  create_table "stock_quotes", 
+  create_table "stock_quotes", :force => true do |
