5c263a4ca82025c001cd2b195855e662016f1a66
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-11-23 07:16:40 +0000

    Adding package.json

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,4 @@ ActiveRecord::Schema.define(:version => 20131130172812) do
     t.string   "name"
     t.string   "specification"
     t.string   "currency_ref"
-    t.decimal  "opening_
+    t.decimal  "opening_price",            
