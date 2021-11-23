72abae80b30ff2896bed668e427ab87022f76321
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-11-23 08:23:44 +0000

    Update import path

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(:version => 20131130172812) do
     t.string   "name"
     t.string   "specification"
     t.string   "currency_ref"
-    t.decimal  "opening_price",             :precision => 10,
+    t.decimal  "opening_price",             :precision => 10, :scale => 2
+    t
