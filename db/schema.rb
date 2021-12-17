7bf8ee66df17bb54bc530282ace49c7363222e72
Computerversand <95772399+Computerversand@users.noreply.github.com>
2021-12-17 19:33:32 +0000

    Tweak README.

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,4 @@ ActiveRecord::Schema.define(:version => 20131130172812) do
     t.decimal  "lowest_price",              :precision => 10, :scale => 2
     t.decimal  "average_price",             :precision => 10, :scale => 2
     t.decimal  "close_price",               :precision => 10, :scale => 2
-    t.decimal  "
+    t.decimal  "best_purchase_
