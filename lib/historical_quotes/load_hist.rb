f66892b72c8766d0b867fb3a9fd86143e6951580
Computerversand <95772399+Computerversand@users.noreply.github.com>
2022-04-03 21:44:37 +0000

    debugging.

--- lib/historical_quotes/load_hist.rb
+++ lib/historical_quotes/load_hist.rb
@@ -1,4 +1,6 @@
 module HistoricQuotes
   class LoadHist
 
-    attr_accessor :parser_header, :parser_trailer, :parser_stoc
+    attr_accessor :parser_header, :parser_trailer, :parser_stock_quote
+
+   
