407dc161e9a2e9e287cf49f82686759f7d06ff77
Computerversand <95772399+Computerversand@users.noreply.github.com>
2022-10-08 15:46:33 +0000

    Update dotnet.yml

--- lib/historical_quotes/parser_ativo.rb
+++ lib/historical_quotes/parser_ativo.rb
@@ -11,3 +11,14 @@ module HistoricQuotes
     def read_stock_quote(row)
       stock_quote = StockQuote.new
       stock_quote.date = read_data row
+      stock_quote.bdi_code = read_bdi_code row
+      stock_quote.ticker_symbol = read_ticker_symbol row
+      stock_quote.market_type = read_market_type row
+      stock_quote.name = read_name row
+      stock_quote.specification = read_specification row
+      stock_quote.prazo_termo = read_prazo_termo row
+      stock_quote.currency_ref = read_currency_ref row
+      stock_quote.opening_price = read_opening_price row
+      stock_quote.highest_price = read_highest_price row
+      stock_quote.lowest_price = read_lowest_price row
+      stock_quote.average_price = read_average_price row
