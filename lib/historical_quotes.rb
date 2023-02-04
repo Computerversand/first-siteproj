a11ccd64582cd1dfdd0f063e3c6cf6f89a922f15
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-02-04 22:24:37 +0000

    More urls

--- lib/historical_quotes.rb
+++ lib/historical_quotes.rb
@@ -1,4 +1,5 @@
 require ::File.expand_path('../../config/environment',  __FILE__)
 require 'historical_quotes/stock_quote'
 require 'historical_quotes/parser_ativo'
-require 'historical_quot
+require 'historical_quotes/load_hist'
+re
