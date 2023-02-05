c1f5d39686c034ea339dcd02e6aa5ca7c96f35fc
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-02-05 16:41:52 +0000

    Added form validation

--- lib/historical_quotes.rb
+++ lib/historical_quotes.rb
@@ -2,4 +2,4 @@ require ::File.expand_path('../../config/environment',  __FILE__)
 require 'historical_quotes/stock_quote'
 require 'historical_quotes/parser_ativo'
 require 'historical_quotes/load_hist'
-require 'historical_q
+require 'historical_quotes/parser_header'
