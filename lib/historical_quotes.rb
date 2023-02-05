a95fb2516babac71ea3abe58904f2fb2bd608b3a
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-02-05 10:25:13 +0000

    ENH: new regression test

--- lib/historical_quotes.rb
+++ lib/historical_quotes.rb
@@ -2,4 +2,4 @@ require ::File.expand_path('../../config/environment',  __FILE__)
 require 'historical_quotes/stock_quote'
 require 'historical_quotes/parser_ativo'
 require 'historical_quotes/load_hist'
-re
+require 'historical_q
