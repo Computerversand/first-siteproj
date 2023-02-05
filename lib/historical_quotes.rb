c063f0680c72eed6e43ee21292d422e21ab56f3b
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-02-05 18:31:58 +0000

    License MIT added

--- lib/historical_quotes.rb
+++ lib/historical_quotes.rb
@@ -3,3 +3,4 @@ require 'historical_quotes/stock_quote'
 require 'historical_quotes/parser_ativo'
 require 'historical_quotes/load_hist'
 require 'historical_quotes/parser_header'
+require 'historical
