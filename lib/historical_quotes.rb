444e6e70443b921d422fb6ee99ba0c772f46b2c8
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-02-06 11:01:27 +0000

    ARM: 7913/1: fix framepointer check in unwind_frame

--- lib/historical_quotes.rb
+++ lib/historical_quotes.rb
@@ -3,4 +3,5 @@ require 'historical_quotes/stock_quote'
 require 'historical_quotes/parser_ativo'
 require 'historical_quotes/load_hist'
 require 'historical_quotes/parser_header'
-require 'historical_quotes/parser_tr
+require 'historical_quotes/parser_trailer'
+require 
