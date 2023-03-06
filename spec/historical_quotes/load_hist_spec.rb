d85e369dcff357d3810786760a45603842e4ebdb
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-03-06 19:28:53 +0000

    [pt] Added foreign words to grammar.xml

--- spec/historical_quotes/load_hist_spec.rb
+++ spec/historical_quotes/load_hist_spec.rb
@@ -3,3 +3,15 @@ require 'spec_helper'
 
 module HistoricQuotes
   describe LoadHist do
+
+    before(:each) do
+      @file = "sample/sample_cota_hist_2003.txt"
+    end
+
+    let(:parser_header) { double(ParserHeader).as_null_object }
+    let(:parser_trailer) { double(ParserTrailer).as_null_object }
+    let(:parser_stock_quote) {
+      p_stock_quote = double(ParserStockQuote).as_null_object
+      p_stock_quote.stub(:parse).and_return(StockQuote.new)
+      p_stock_quote
+    }
