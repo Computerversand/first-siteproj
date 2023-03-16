f01891109f8b93f24e7b8261b817ff7a5a6017d0
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-03-16 06:03:39 +0000

    Bump Go version

--- spec/historical_quotes/parser_header_spec.rb
+++ spec/historical_quotes/parser_header_spec.rb
@@ -3,4 +3,4 @@ require 'spec_helper'
 module HistoricQuotes
   describe ParserHeader do
     before(:each) do
-      sample_row = "00COTAHIST.2003BOVESPA 20040531                                                                    
+      sample_row = "00COTAHIST.2003BOVESPA 20040531                                                                                    
