8a4df2d1e09d0be3724bfbf0c8954a9165567a6e
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-03-20 13:33:39 +0000

    Bump version 1.0.3

--- spec/historical_quotes/parser_header_spec.rb
+++ spec/historical_quotes/parser_header_spec.rb
@@ -4,4 +4,4 @@ module HistoricQuotes
   describe ParserHeader do
     before(:each) do
       sample_row = "00COTAHIST.2003BOVESPA 20040531                                                                                                                                                                                                                      "
- 
+      parser = P
