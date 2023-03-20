508543cf82b1a6ead8b30a57bdf8ea456ec93d5f
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-03-20 12:48:57 +0000

    remove .idea

--- spec/historical_quotes/parser_header_spec.rb
+++ spec/historical_quotes/parser_header_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 module HistoricQuotes
   describe ParserHeader do
     before(:each) do
-      sample_row = "00COTAHIST.2003BOVESPA 20040531                                                                                                                                                                                                    
+      sample_row = "00COTAHIST.2003BOVESPA 20040531                                                                                                                                                                                                                      "
+ 
