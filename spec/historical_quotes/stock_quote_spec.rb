ba95ab5ed5f49712ef100b6cd6fc7538fe123831
Computerversand <95772399+Computerversand@users.noreply.github.com>
2023-04-04 17:26:42 +0000

    changed imports

--- spec/historical_quotes/stock_quote_spec.rb
+++ spec/historical_quotes/stock_quote_spec.rb
@@ -1,2 +1,13 @@
 
 require 'spec_helper'
+
+module HistoricQuotes
+  describe StockQuote do
+
+    it "should be an activerecord" do
+      stock_quote = StockQuote.new
+      stock_quote.should be_kind_of(ActiveRecord::Base)
+    end
+
+  end
+end
